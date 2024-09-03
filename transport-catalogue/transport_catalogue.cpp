ce837a33b37f3eebb7c46daebae8851f175d1bc7
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-09-04 00:16:08 +0600

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,1 +1,71 @@
-// место для вашего кода
+#include <unordered_set>
+
+#include "transport_catalogue.h"
+#include "geo.h"
+#include <sstream>
+
+using namespace transport;
+using namespace geo;
+
+std::string_view TransportCatalogue::AddId(const std::string_view id) {
+    ids_.push_front(std::string {id});
+    return {ids_.front().begin(), ids_.front().end()};
+}
+
+void TransportCatalogue::AddStop(const std::string_view id, const Coordinates place) {
+    std::string_view stop_id = AddId(id);
+    stops_.insert({stop_id, {stop_id, place}});
+    busses4stop_.insert({stop_id, {}});
+}
+
+void TransportCatalogue::AddBus(const std::string_view id, const std::vector<std::string_view> stops) {
+    std::string_view bus_id = AddId(id);
+    std::vector<std::string_view> stops_ids;
+    stops_ids.reserve(stops.size());
+    for (const auto& s : stops) {
+        stops_ids.push_back(stops_.at(s).id);
+        busses4stop_[stops_ids.back()].insert(bus_id);
+    }
+    busses_.insert({bus_id, {bus_id, std::move(stops_ids)}});
+}
+
+const BusDescription* TransportCatalogue::GetBus(const std::string_view id) const {
+    if (busses_.count(id)) {
+        return &busses_.at(id);
+    }
+    return nullptr;
+}
+
+const std::optional<RouteStatistics> TransportCatalogue::GetStat(const BusDescription* bus) const {
+    if (bus) {
+        double route_length = 0.0;
+        std::unordered_set<std::string_view> unique_stops;
+        std::optional<StopDescription> prev_stop;
+        for (const std::string_view& s : bus->stops) {
+            const auto stop = stops_.at(s);
+            unique_stops.insert(s);
+            if (prev_stop) {
+                route_length += ComputeDistance(prev_stop->place, stop.place);
+            }
+            prev_stop = stop;
+        }
+        return RouteStatistics {route_length, bus->stops.size(), unique_stops.size()};
+    }
+    return std::nullopt;
+}
+
+const std::vector<std::string_view> TransportCatalogue::GetBusses4Stop(const std::string_view id) const {
+    std::vector<std::string_view> result;
+    if (busses4stop_.count(id)) {
+        const auto& busses = busses4stop_.at(id);
+        result.reserve(busses.size());
+        for (const auto& b : busses) {
+            result.push_back(b);
+        }
+    } else {
+        std::stringstream msg;
+        msg << "Stop " << id << ": not found";
+        throw std::out_of_range(msg.str());
+    }
+    return result;
+}
