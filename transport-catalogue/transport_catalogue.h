d6009e346bace381507f9a303743fca53f1a8dc0
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-09-04 00:16:27 +0600

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,48 @@
-// место для вашего кода
+#pragma once
+
+#include <vector>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <optional>
+#include <unordered_map>
+#include <iostream>
+#include <forward_list>
+
+#include "geo.h"
+
+namespace transport {
+
+    struct StopDescription {
+        std::string_view id;
+        geo::Coordinates place;
+    };
+
+    struct BusDescription {
+        std::string_view id;
+        std::vector<std::string_view> stops;
+    };
+
+    struct RouteStatistics {
+        double length = 0.0;
+        size_t stops_count = 0;
+        size_t unique_stops = 0;
+    };
+
+    class TransportCatalogue {
+    public:
+        void AddStop(const std::string_view id, const geo::Coordinates place);
+        void AddBus(const std::string_view id, std::vector<std::string_view> stops);
+        const BusDescription* GetBus(const std::string_view id) const;
+        const std::optional<RouteStatistics> GetStat(const BusDescription* bus) const;
+        const std::vector<std::string_view> GetBusses4Stop(const std::string_view id) const;
+    private:
+        std::string_view AddId(const std::string_view id);
+    private:
+        std::forward_list<std::string> ids_;
+        std::unordered_map<std::string_view, StopDescription> stops_;
+        std::unordered_map<std::string_view, BusDescription> busses_;
+        std::unordered_map<std::string_view, std::set<std::string_view>> busses4stop_;
+    };
+    
+}
