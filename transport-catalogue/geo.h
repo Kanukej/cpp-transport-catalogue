01d8b01b4308bb90a78e8ea08b0bdd6138946320
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-09-04 09:11:18 +0600

    Update geo.h add consts

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -4,6 +4,9 @@
 
 namespace geo {
 
+    static constexpr int const& EarthRadius = 6371000;
+    static constexpr double const& Delta = 3.1415926535 / 180.;
+
     struct Coordinates {
         double lat;
         double lng;
@@ -20,9 +23,8 @@ namespace geo {
         if (from == to) {
             return 0;
         }
-        static const double dr = 3.1415926535 / 180.;
-        return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                   + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr)) * 6371000;
+        return acos(sin(from.lat * Delta) * sin(to.lat * Delta)
+                   + cos(from.lat * Delta) * cos(to.lat * Delta) * cos(abs(from.lng - to.lng) * Delta)) * EarthRadius;
     }
 
 }
