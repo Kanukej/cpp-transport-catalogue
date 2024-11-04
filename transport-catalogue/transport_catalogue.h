c7878a897ba6470a12d3fd9a7badaac54d00a703
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-11-05 04:39:33 +0600

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <unordered_set>
 #include <set>
 #include <stdexcept>
 #include <string>
@@ -10,16 +11,14 @@
 #include <forward_list>
 
 #include "geo.h"
+#include "domain.h"
 
 namespace transport {
     
     using StopsMap = std::unordered_map<std::string_view, int>;
-    
-    struct Dist2Stop {
-        std::string_view stop;
-        int distance;
-    };
+    using BusPtr = std::string_view;
 
+    
     struct StopDescription {
         std::string_view id;
         geo::Coordinates place;
@@ -29,6 +28,7 @@ namespace transport {
     struct BusDescription {
         std::string_view id;
         std::vector<std::string_view> stops;
+        std::vector<std::string_view> final_stops;
     };
 
     struct RouteStatistics {
@@ -37,21 +37,55 @@ namespace transport {
         size_t unique_stops = 0;
         double curvature = 0.0;
     };
-
+    
+    using StopContainer = std::unordered_map<std::string_view, StopDescription>;
+    using BusContainer = std::unordered_map<std::string_view, BusDescription>;
+    
+    class TransportStopIterator {
+    public:
+        TransportStopIterator() = default;
+        TransportStopIterator(const StopContainer::const_iterator& it) : it_(it) {
+            //
+        }
+    
+        const geo::Coordinates& operator*() const {
+            return it_->second.place;
+        }
+        const geo::Coordinates* operator->() const {
+            return &it_->second.place;
+        }
+        TransportStopIterator& operator++() {
+            ++it_;
+            return *this;
+        }
+        bool operator==(const TransportStopIterator& other) {
+            return it_ == other.it_;
+        }
+        bool operator!=(const TransportStopIterator& other) {
+            return it_ != other.it_;
+        }
+    private:
+        StopContainer::const_iterator it_;
+    };
+    
     class TransportCatalogue {
     public:
         void AddStop(const std::string_view id, const geo::Coordinates place);
-        void AddBus(const std::string_view id, std::vector<std::string_view> stops);
+        void AddBus(const std::string_view id, const std::vector<std::string_view>& stops, const std::vector<std::string_view>& final_stops);
         void AddDistance(const std::string_view from, const std::string_view to, const int dists);
         const BusDescription* GetBus(const std::string_view id) const;
         const std::optional<RouteStatistics> GetStat(const BusDescription* bus) const;
-        const std::set<std::string_view>& GetBusses4Stop(const std::string_view id) const;
+        const std::set<BusPtr>* GetBusses4Stop(const std::string_view id) const;
+        std::vector<geo::Coordinates> GetStops() const;
+        std::set<std::string_view> GetStopIds() const;
+        std::vector<std::string_view> GetBuses() const;
+        const StopDescription* GetStop(const std::string_view id) const;
     private:
         std::string_view AddId(const std::string_view id);
     private:
         std::forward_list<std::string> ids_;
-        std::unordered_map<std::string_view, StopDescription> stops_;
-        std::unordered_map<std::string_view, BusDescription> busses_;
+        StopContainer stops_;
+        BusContainer buses_;
         std::unordered_map<std::string_view, std::set<std::string_view>> busses4stop_;
     };
     
