5b128a907088fe856fd6d7f3d26afa2610075b33
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-12-25 18:17:33 +0600

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -37,12 +37,79 @@ void TransportCatalogue::AddDistance(const std::string_view from, const std::str
     stops_.at(from).distances[stops_.at(to).id] = dist;
 }
 
+void TransportCatalogue::BuildMapGraph(const int velocity, const TimeUnit time) {
+    std::vector<const StopDescription*> descr;
+    for (const auto& [name, sd] : stops_) {
+        const auto idx = std::pair<graph::VertexId, graph::VertexId> {stop_map_.AddVertex(), stop_map_.AddVertex()} ;
+        map2stop_[name] = idx;
+        stop2map_[idx.first] = name;
+        descr.push_back(&sd);
+        stop_map_.AddEdge({idx.first, idx.second, time});
+    }
+    for (const auto& [id, bus] : buses_) {
+        if (bus.stops.empty()) {
+            continue;
+        }
+        std::vector<const DoubleStop*> graph_stops;
+        graph_stops.reserve(bus.stops.size());
+        for (auto from_it = bus.stops.begin(); from_it != bus.stops.end(); ++from_it) {
+            const auto& from = map2stop_.at(*from_it);
+            graph_stops.push_back(&from);
+        }
+        for (size_t from = 0; from < graph_stops.size() - 1; ++from) {
+            double travel_time = 0.;
+            for (size_t to = from + 1; to < graph_stops.size(); ++to) {
+                const auto dist = GetDistance(*descr.at(graph_stops.at(to - 1)->first / 2), *descr.at(graph_stops.at(to)->first / 2));
+                if (dist) {
+                    travel_time += (60 * static_cast<double>(dist.value()) / velocity) / 1000;
+                    bus2map_[stop_map_.AddEdge({graph_stops.at(from)->second, graph_stops.at(to)->first, travel_time})] = {id, to - from};
+                }
+            }
+        }
+    }
+}
+
+std::optional<RouteDescription> TransportCatalogue::DescribePath(const graph::EdgeId& id) const {
+    const auto& edge = stop_map_.GetEdge(id);
+    if (edge.weight > TimeUnit {0}) {
+        const auto stop_it = stop2map_.find(edge.from);
+        if (stop_it != stop2map_.end()) {
+            RouteDescription ans {PathType::Wait, edge.weight, stop_it->second, std::nullopt};
+            return ans;
+        } else {
+            const auto span_bus = bus2map_.at(id);
+            RouteDescription ans {PathType::Bus, edge.weight, span_bus.first, span_bus.second};
+            return ans;
+        }
+    }
+    return std::nullopt;
+}
+
+graph::VertexId TransportCatalogue::GetStopGraphId(const std::string_view& id) const {
+    return map2stop_.at(id).first;
+}
+
+const graph::DirectedWeightedGraph<TimeUnit>& TransportCatalogue::GetMapGraph() const {
+    return stop_map_;
+}
+
 const BusDescription* TransportCatalogue::GetBus(const std::string_view id) const {
     auto bus_ptr = buses_.find(id);
     if (bus_ptr != buses_.end()) {
         return &bus_ptr->second;
     }
     return nullptr;
+}
+
+std::optional<int> TransportCatalogue::GetDistance(const StopDescription& from, const StopDescription& to) const {
+    const auto& distances = from.distances;
+    auto dist_ptr = distances.find(to.id);
+    if (dist_ptr != distances.end()) {
+        return dist_ptr->second;
+    } else if (to.distances.end() != (dist_ptr = to.distances.find(from.id))) {
+        return dist_ptr->second;
+    }
+    return std::nullopt;
 }
 
 const std::optional<RouteStatistics> TransportCatalogue::GetStat(const BusDescription* bus) const {
@@ -55,12 +122,9 @@ const std::optional<RouteStatistics> TransportCatalogue::GetStat(const BusDescri
             const auto stop = stops_.at(s);
             unique_stops.insert(s);
             if (prev_stop) {
-                const auto& distances = prev_stop->distances;
-                auto dist_ptr = distances.find(s);
-                if (dist_ptr != distances.end()) {
-                    route_dist += dist_ptr->second;
-                } else if (stop.distances.end() != (dist_ptr = stop.distances.find(prev_stop->id))) {
-                    route_dist += dist_ptr->second;
+                const auto dist = GetDistance(*prev_stop, stop);
+                if (dist) {
+                    route_dist += dist.value();
                 } else {
                     std::stringstream ss;
                     ss << "distance between stop " << s << " and stop " << prev_stop->id << " not found in base";
