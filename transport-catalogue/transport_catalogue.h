d960cf161a93693b01ed31e07c36a3ed661ece76
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-12-25 18:17:50 +0600

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include <vector>
@@ -10,6 +11,7 @@
 #include <iostream>
 #include <forward_list>
 
+#include "graph.h"
 #include "geo.h"
 #include "domain.h"
 
@@ -36,6 +38,25 @@ namespace transport {
         size_t stops_count = 0;
         size_t unique_stops = 0;
         double curvature = 0.0;
+    };
+    
+    enum class PathType {
+        Wait,
+        Bus
+    };
+    
+    using TimeUnit = double;
+    
+    struct RouteDescription {
+        PathType type;
+        TimeUnit time;
+        std::string_view id;
+        std::optional<int> span;
+    };
+    
+    struct PathDescription {
+        TimeUnit time;
+        std::vector<RouteDescription> route;
     };
     
     using StopContainer = std::unordered_map<std::string_view, StopDescription>;
@@ -69,10 +90,13 @@ namespace transport {
     };
     
     class TransportCatalogue {
+        using DoubleStop = std::pair<graph::VertexId, graph::VertexId>;
+        using SpanBus = std::pair<std::string_view, size_t>;
     public:
         void AddStop(const std::string_view id, const geo::Coordinates place);
         void AddBus(const std::string_view id, const std::vector<std::string_view>& stops, const std::vector<std::string_view>& final_stops);
         void AddDistance(const std::string_view from, const std::string_view to, const int dists);
+        void BuildMapGraph(const int velocity, const TimeUnit time);
         const BusDescription* GetBus(const std::string_view id) const;
         const std::optional<RouteStatistics> GetStat(const BusDescription* bus) const;
         const std::set<BusPtr>* GetBusses4Stop(const std::string_view id) const;
@@ -80,13 +104,22 @@ namespace transport {
         std::set<std::string_view> GetStopIds() const;
         std::vector<std::string_view> GetBuses() const;
         const StopDescription* GetStop(const std::string_view id) const;
+        const graph::DirectedWeightedGraph<TimeUnit>& GetMapGraph() const;
+        graph::VertexId GetStopGraphId(const std::string_view& id) const;
+        std::optional<RouteDescription> DescribePath(const graph::EdgeId& edge) const;
     private:
         std::string_view AddId(const std::string_view id);
+        std::optional<int> GetDistance(const StopDescription& from, const StopDescription& to) const;
     private:
         std::forward_list<std::string> ids_;
         StopContainer stops_;
         BusContainer buses_;
         std::unordered_map<std::string_view, std::set<std::string_view>> busses4stop_;
+        
+        std::unordered_map<std::string_view, DoubleStop> map2stop_;
+        std::unordered_map<graph::VertexId, std::string_view> stop2map_;
+        std::unordered_map<graph::EdgeId, SpanBus> bus2map_;
+        graph::DirectedWeightedGraph<TimeUnit> stop_map_;
     };
     
 }
