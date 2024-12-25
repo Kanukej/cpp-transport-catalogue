60cd1c80bf1ff5912e372f13e2c0fa09560598ac
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-12-25 18:17:18 +0600

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -2,22 +2,24 @@
 
 #include "transport_catalogue.h"
 #include "map_renderer.h"
+#include "transport_router.h"
 #include "json.h"
 
 namespace handler {
     
 class CatalogueConstructor {
 public:
-    CatalogueConstructor(transport::TransportCatalogue& db);
+    CatalogueConstructor(transport::TransportCatalogue& db, const domain::RoutingSettings& settings);
     void FillFromCommands(const domain::Commands& commands);
 private:
     transport::TransportCatalogue& db_;
+    const domain::RoutingSettings& settings_;
 };
 
 class RequestHandler {
 public:
     // MapRenderer понадобится в следующей части итогового проекта
-    RequestHandler(const transport::TransportCatalogue& db, const renderer::MapRenderer& renderer);
+    RequestHandler(const transport::TransportCatalogue& db, const renderer::MapRenderer& renderer, const router::TransportRouter& router);
 
     // Возвращает информацию о маршруте (запрос Bus)
     std::optional<transport::RouteStatistics> GetBusStat(const std::string_view& bus_name) const;
@@ -34,6 +36,7 @@ private:
     // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
     const transport::TransportCatalogue& db_;
     const renderer::MapRenderer& renderer_;
+    const router::TransportRouter& router_;
 };
     
 }
