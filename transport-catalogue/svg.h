a3fc530579e74a34577993d99b7bec67d7c0426c
Родионова Алина <160567252+Kanukej@users.noreply.github.com>
2025-05-19 01:20:26 +0600

    Update svg.h
    deleted unnecessary comments

--- transport-catalogue/svg.h
+++ transport-catalogue/svg.h
@@ -48,7 +48,6 @@ public:
     }
     
     virtual ~ObjectContainer() {
-        //
     }
     
 };
@@ -57,7 +56,6 @@ class Drawable {
 public:
     virtual void Draw(ObjectContainer &container) const = 0;
     virtual ~Drawable() {
-        //
     }
 };
 
@@ -72,10 +70,7 @@ struct Point {
     double y = 0;
 };
 
-/*
- * Вспомогательная структура, хранящая контекст для вывода SVG-документа с отступами.
- * Хранит ссылку на поток вывода, текущее значение и шаг отступа при выводе элемента
- */
+
 struct RenderContext {
     RenderContext(std::ostream& out)
         : out(out) {
@@ -102,11 +97,6 @@ struct RenderContext {
     int indent = 0;
 };
 
-/*
- * Абстрактный базовый класс Object служит для унифицированного хранения
- * конкретных тегов SVG-документа
- * Реализует паттерн "Шаблонный метод" для вывода содержимого тега
- */
 class Object {
 public:
     void Render(const RenderContext& context) const;
@@ -117,10 +107,6 @@ private:
     virtual void RenderObject(const RenderContext& context) const = 0;
 };
 
-/*
- * Класс Circle моделирует элемент <circle> для отображения круга
- * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle
- */
 
 template<class T>
 class PathProps {
@@ -166,11 +152,6 @@ private:
     Point center_;
     double radius_ = 1.0;
 };
-
-/*
- * Класс Polyline моделирует элемент <polyline> для отображения ломаных линий
- * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polyline
- */
         
         
 class Polyline final : public Object, public PathProps<Polyline> {
@@ -178,41 +159,26 @@ public:
     // Добавляет очередную вершину к ломаной линии
     Polyline& AddPoint(Point point);
 
-    /*
-     * Прочие методы и данные, необходимые для реализации элемента <polyline>
-     */
     void RenderObject(const RenderContext& context) const override;
 private:
     std::vector<Point> points_;
 };
-
-/*
- * Класс Text моделирует элемент <text> для отображения текста
- * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
- */
-        
+   
         
 class Text final : public Object, public PathProps<Text> {
 public:
-    // Задаёт координаты опорной точки (атрибуты x и y)
     Text& SetPosition(Point pos);
 
-    // Задаёт смещение относительно опорной точки (атрибуты dx, dy)
     Text& SetOffset(Point offset);
 
-    // Задаёт размеры шрифта (атрибут font-size)
     Text& SetFontSize(uint32_t size);
 
-    // Задаёт название шрифта (атрибут font-family)
     Text& SetFontFamily(std::string font_family);
 
-    // Задаёт толщину шрифта (атрибут font-weight)
     Text& SetFontWeight(std::string font_weight);
 
-    // Задаёт текстовое содержимое объекта (отображается внутри тега text)
     Text& SetData(std::string data);
 
-    // Прочие данные и методы, необходимые для реализации элемента <text>
     void RenderObject(const RenderContext& context) const override;
 private:
     std::string data_;
@@ -227,13 +193,10 @@ private:
 class Document : public ObjectContainer {
 public:
 
-    // Добавляет в svg-документ объект-наследник svg::Object
     void AddPtr(std::unique_ptr<Object>&& obj) override;
 
-    // Выводит в ostream svg-представление документа
     void Render(std::ostream& out) const;
 
-    // Прочие методы и данные, необходимые для реализации класса Document
 private:
     std::vector<std::unique_ptr<Object>> objects_;
 };
@@ -248,7 +211,6 @@ public:
         , p3_(p3) {
     }
 
-    // Реализует метод Draw интерфейса svg::Drawable
     void Draw(svg::ObjectContainer& container) const override;
 
 private:
@@ -259,7 +221,6 @@ class Star : public svg::Drawable {
 public:
     Star(svg::Point center, double outer_rad, double inner_rad, int num_rays) :
         center_(center), outer_rad_(outer_rad), inner_rad_(inner_rad), num_rays_(num_rays) {
-        //
     }
     
     void Draw(svg::ObjectContainer& container) const override;
@@ -273,7 +234,6 @@ private:
 class Snowman : public svg::Drawable {
 public:
     Snowman(svg::Point center, double rad) : center_(center), rad_(rad) {
-        //
     }
     
     void Draw(svg::ObjectContainer& container) const override;
